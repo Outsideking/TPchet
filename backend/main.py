01fb0d2b86fc19dfb5e15b8d1505a18cf8c663f8
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-09-19 11:11:10 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from routes import auth, admin, payment, translation
 
+app = FastAPI(title="TPchet API")
+
+app.include_router(auth.router, prefix="/auth")
+app.include_router(admin.router, prefix="/admin")
+app.include_router(payment.router, prefix="/payment")
+app.include_router(translation.router, prefix="/translate")
