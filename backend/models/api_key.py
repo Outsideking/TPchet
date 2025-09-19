0fb57570c54ce6acc1987c1daeaa9b91db7307ef
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-09-19 11:06:34 +0700

    Update api_key.py

--- backend/models/api_key.py
+++ backend/models/api_key.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from db.database import Base
 
+class APIKey(Base):
+    __tablename__ = "api_keys"
+
+    id = Column(Integer, primary_key=True, index=True)
+    key = Column(String, unique=True, index=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    quota = Column(Integer, default=1000)
+    usage = Column(Integer, default=0)
