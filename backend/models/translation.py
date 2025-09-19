3eb9f7e64c0874c2722cf7db1e3b9a66d1c58531
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-09-19 11:07:15 +0700

    Update translation.py

--- backend/models/translation.py
+++ backend/models/translation.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
+from sqlalchemy.sql import func
+from db.database import Base
 
+class Translation(Base):
+    __tablename__ = "translations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    source_text = Column(Text, nullable=False)
+    translated_text = Column(Text, nullable=False)
+    source_lang = Column(String(10))
+    target_lang = Column(String(10))
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
